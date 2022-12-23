80227982e2367d8ec4a94df8e8d2964a6cc55f50
이준협 <leejh0815@ksmartech.com>
2022-12-24 01:59:30 +0900

    pod 수정

---
+++ KConnect/Pods/Target Support Files/Pods-KConnect-KConnectUITests/Pods-KConnect-KConnectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KConnect_KConnectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KConnect_KConnectUITests
+@end
---
+++ KConnect/Pods/Target Support Files/Pods-KConnectTests/Pods-KConnectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KConnectTests : NSObject
+@end
+@implementation PodsDummy_Pods_KConnectTests
+@end
