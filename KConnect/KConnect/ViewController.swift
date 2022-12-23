889061d2c7a105df5452d07c12a3f1d7ed518a39
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2022-12-24 01:12:41 +0900

    Add files via upload
    1.로그인화면 생성(진행중)

---
+++ KConnect/KConnect/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  KConnect
+//
+//  Created by 이준협 on 2022/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginId: UITextField!
+    @IBOutlet weak var loginPwd: UITextField!
+    @IBOutlet weak var autoLoginCheck: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    var autoLoginFlag = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func loginBtnAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func autoLoginBtnAction(_ sender: Any) {
+        
+        if autoLoginFlag { //체크 된상태
+            autoLoginCheck.setImage(UIImage(systemName: "squareshape"), for: .normal)
+            autoLoginFlag = false
+        }else { //체크 풀린상태
+            autoLoginCheck.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+            autoLoginFlag = true
+        }
+    }
+    
+    
+    
+    
+
+
+}
+
